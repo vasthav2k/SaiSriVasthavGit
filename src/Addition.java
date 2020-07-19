911833f3c687e0480aa81bf2f49b15502684c76d
vasthav2k <44031833+vasthav2k@users.noreply.github.com>
2020-07-19 21:22:49 +0530

    Updated Task1

---
+++ src/Addition.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Addition
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter 1st Number:");
+		int a=sc.nextInt();
+		System.out.println("Enter 2nd Number:");
+		int b=sc.nextInt();
+		int res=a+b;
+		System.out.println("Result of adding "+a+" and "+ b + " is :"+res);
+	}
+}
